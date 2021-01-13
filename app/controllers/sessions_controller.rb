26b4f8118132d00cb5a09280b1efd2d8f49fe27c
giegeralexandra <giegeralexandra@gmail.com>
2021-01-12 21:05:18 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    if session[:name] != "" 
+      redirect_to '/'
+    end
+  end
+
+  def create 
+    if !params[:name] || params[:name].empty?
+      redirect_to '/login'
+    else 
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy 
+    session.delete :name 
+  end
+
+end
